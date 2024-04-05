f67d9123145f4bce02293fa1d2ed85754fe97505
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-04-05 15:14:56 +0700

    menampilkan tampilan yes or no pada hasil kodingan

--- ActivitySearch/ActivitySearch.cpp
+++ ActivitySearch/ActivitySearch.cpp
@@ -55,7 +55,10 @@ void LinearSerach()
 
 		if (i == n) // step 5
 			cout << "\n" << item << " not found in the array\n";
-		cout << "\nNumber of comparisons: " << ctr << end;
+		cout << "\nNumber of comparisons: " << ctr << endl;
 
-	}
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+
+	} while
 }
