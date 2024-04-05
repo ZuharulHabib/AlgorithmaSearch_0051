c02f5914a583a5c64d79486a10b298b368c0fe94
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-04-05 14:58:01 +0700

    membuat fungsi

--- ActivitySearch/ActivitySearch.cpp
+++ ActivitySearch/ActivitySearch.cpp
@@ -4,3 +4,17 @@ using namespace std;
 int arr[20] //array to be searched
 int n; //Number of element in the array
 int i; // index of array element 
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of element in the arrat: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 an maximum 20 element. \n\n";
+
+	}
+}
